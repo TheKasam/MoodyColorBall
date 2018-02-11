80ad02d0f17b573db54aa6704eeea7b49c8c8e96
Sai Kasam <saikasam98@gmail.com>
2018-02-11 17:18:21 -0600

    added circle animation

--- MoodyColorBall/GameScene.swift
+++ MoodyColorBall/GameScene.swift
@@ -954,6 +954,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         fbIcon()
         starIcon()
         shareIcon()
+        animateCircle()
         
     }
     
@@ -962,6 +963,57 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
             childNode(withName: name)?.removeFromParent()
         }
         
+    }
+    
+    func animateCircleBorder(){
+
+    }
+    func animateCircle() {
+        let circleLayer: CAShapeLayer!
+        
+        
+        
+        // Use UIBezierPath as an easy way to create the CGPath for the layer.
+        // The path should be the entire circle.
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: (frame.size.width - 10)/2, startAngle: 0.0, endAngle: CGFloat(Double.pi * 2.0), clockwise: true)
+        
+        // Setup the CAShapeLayer with the path, colors, and line width
+        circleLayer = CAShapeLayer()
+        circleLayer.path = circlePath.cgPath
+        circleLayer.backgroundColor = UIColor.clear.cgColor
+        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.strokeColor = UIColor.red.cgColor
+        circleLayer.lineWidth = 5.0;
+        
+        // Don't draw the circle initially
+        circleLayer.strokeEnd = 0.0
+        
+        // Add the circleLayer to the view's layer's sublayers
+        view?.layer.addSublayer(circleLayer)
+        
+        
+        
+        let duration: TimeInterval = 5
+        
+        // We want to animate the strokeEnd property of the circleLayer
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        // Set the animation duration appropriately
+        animation.duration = duration
+        
+        // Animate from 0 (no circle) to 1 (full circle)
+        animation.fromValue = 0
+        animation.toValue = 1
+        
+        // Do a linear animation (i.e. the speed of the animation stays the same)
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        
+        // Set the circleLayer's strokeEnd property to 1.0 now so that it's the
+        // right value when the animation ends.
+        circleLayer.strokeEnd = 1.0
+        
+        // Do the actual animation
+        circleLayer.add(animation, forKey: "animateCircle")
     }
     
     
